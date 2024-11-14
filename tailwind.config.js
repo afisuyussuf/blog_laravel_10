db32a01c95387559e7468df0c416c5268d2b1b1d
afisuyussuf <afisuyussuf370@gmail.com>
2024-11-14 11:46:27 +0000

    Blog versio dynamique

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+    strategy: 'class', // only generate classes
+  }),
+  ],
+}
+
