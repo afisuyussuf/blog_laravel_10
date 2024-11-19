3b12c7663124071a6540dd8d1cf47b33b5a0783f
afisuyussuf <afisuyussuf370@gmail.com>
2024-11-19 23:25:00 +0000

    [21_38] Laravel 10 __ Le formulaire de recherche

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,22 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class PostController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
+    {
+        $posts = Post::query();
+
+        if ($search = $request->search) {
+            $posts->where(fn(Builder $query) => $query->where('title', 'LIKE', '%' . $search . '%')->orWhere('content', 'LIKE', '%' . $search . '%'));
+        }
+
+        return view('posts.index', [
+            'posts' => $posts->latest()->paginate(5),
+        ]);
+    }
+
+    public function postsByCategory(Category $category): View
     {
         return view('posts.index', [
-            'posts' => Post::latest()->paginate(5),
+            //'posts' => $category->posts()->latest()->paginate(5),
+            'posts' => Post::where('category_id', $category->id)
+                ->latest()
+                ->paginate(5),
+        ]);
+    }
+
+    public function postsByTag(Tag $tag): View
+    {
+        return view('posts.index', [
+            //'posts' => $tag->posts()->latest()->paginate(5),
+            'posts' => Post::whereRelation('tags', 'tags.id', $tag->id)
+                ->latest()
+                ->paginate(5),
         ]);
     }
 
     public function show(Post $post): View
     {
-        return view('posts.show', [
-            'post' => $post,
-        ]);
+        return view('posts.show', ['post' => $post]);
     }
 }
